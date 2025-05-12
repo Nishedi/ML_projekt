937f893a885c558bae0f18e759411bfdc4b74ec5
Nishedi <263948@student.pwr.edu.pl>
2025-05-12 13:41:15 +0200

    przed kroswalidacja

--- ML_projektv3/main.py
+++ ML_projektv3/main.py
@@ -5,11 +5,10 @@ from utils import compute_accuracy, precision, recall, f1_score, standard_scale,
 from OvRClassifier import OvRClassifier
 from Perceptron import Perceptron
 import pandas as pd
-# Dane
+
 withStandardScaler = True
 withVarianceThreshold = True
-def test(withStandardScaler=True, withVarianceThreshold=True, isPloting=False):
-    print("Progowanie? ", withVarianceThreshold, " Skalowanie? ",withStandardScaler)
+def test(isSvm=False,withStandardScaler=True, withVarianceThreshold=True, isPloting=False):
     data = load_wine()
     X = pd.DataFrame(data.data, columns=data.feature_names)
     y = pd.Series(data.target)
@@ -20,15 +19,18 @@ def test(withStandardScaler=True, withVarianceThreshold=True, isPloting=False):
         X = standard_scale(X)
 
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Model OvR
-    model = OvRClassifier(base_class=Perceptron, learning_rate=0.01, n_iter=1000)
+    model = None
+    if isSvm:
+        model = svm.SVC(kernel='linear', C=1.0, random_state=42)
+    else:
+        model = OvRClassifier(base_class=Perceptron, learning_rate=0.01, n_iter=1000)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    #
-    # Metryki
 
+    # Metryki
     accuracy = compute_accuracy(y_test, y_pred)
     _precision = precision(y_test, y_pred, average='macro')
     _recall = recall(y_test, y_pred, average='macro')
@@ -43,26 +45,27 @@ def test(withStandardScaler=True, withVarianceThreshold=True, isPloting=False):
         print("F1 Score:", f1)
 
     return accuracy, _precision, _recall, f1
+
+
 def check_impact_of_variance_and_scaling():
-    x, y, w, z = 0,0
+    x, y, w, z = 0,0,0,0
     num_of_rep = 20
     for i in range(20):
-        acc, _,_,_= test(True, True)
+        acc, _,_,_= test(False,True, True)
         x += acc
     for i in range(20):
-        acc, _,_,_= test(True, False)
+        acc, _,_,_= test(False,True, False)
         y += acc
     for i in range(20):
-        acc, _,_,_= test(False, True)
+        acc, _,_,_= test(False,False, True)
         w += acc
     for i in range(20):
-        acc, _,_,_= test(False, False)
+        acc, _,_,_= test(False,False, False)
         z += acc
-
 
     print("Srednia accuracy dla znormalizowanych danych: ", x/num_of_rep)
     print("Srednia accuracy dla nienormalizowanych danych: ", y/num_of_rep)
     print("Srednia accuracy dla znormalizowanych danych z progowaniem: ", w/num_of_rep)
     print("Srednia accuracy dla nienormalizowanych danych z progowaniem: ", z/num_of_rep)
 
-
+check_impact_of_variance_and_scaling()
